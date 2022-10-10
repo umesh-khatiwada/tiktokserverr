47e6d4cafdaa65190b0ee4dbf4924f244b49a4de
umesh121212 <umesh.khatiwada143@gmail.com>
2022-10-10 15:24:31 +0545

    first commit

--- db/conn.js
+++ db/conn.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const DB = process.env.DATABASE;
+// const DB = process.env.DATABASE;
 
-mongoose.connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify:false
-}).then(() => {
-    console.log(`connnection successful`);
-}).catch((err) => console.log(`no connection`));
+// mongoose.connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify:false
+// }).then(() => {
+//     console.log(`connnection successful`);
+// }).catch((err) => console.log(`no connection`));
